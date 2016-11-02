08a6edfebf6c73eeb8880d483a7a72c1722c5873
Clayton Smith <cps5aa@virginia.edu>
2016-11-02 18:18:06 -0400

    Made Attack and Move button move
    I added the &&this.moveMap

--- Game/Assets/Scripts/Map.cs
+++ Game/Assets/Scripts/Map.cs
@@ -1,163 +1,173 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Map : MonoBehaviour {
-    public bool firstPlayerTurn = true;
-    public GameObject selectedUnit;
-    public GameObject TileMove;
-    public GameObject TileAttack;
-    public TileType[] tileType;
-    public ClickUnit cc;
-    public Texture2D background;
-    public GameObject[] moveTiles;
-    public GameObject[] attackTiles; 
-  	private ClickTile[,] clickTiles;
-    public GameObject[] player1Units;
-    public GameObject[] player2Units;
-    public bool moveMode = true;
-    int[,] tiles;
-   // int[,] moveTiles;
-    int sizeX = 18; //Manually change for map
-    int sizeY = 18;
-    int tileSizeX = 200;
-    int tileSizeY = 200;
-    int player1Morale = 100, player2Morale = 100;
+public class Map : MonoBehaviour
+{
+	public bool firstPlayerTurn = true;
+	public GameObject selectedUnit;
+	public GameObject TileMove;
+	public GameObject TileAttack;
+	public TileType[] tileType;
+	public ClickUnit cc;
+	public Texture2D background;
+	public GameObject[] moveTiles;
+	public GameObject[] attackTiles;
+	private ClickTile[,] clickTiles;
+	public GameObject[] player1Units;
+	public GameObject[] player2Units;
 
-    void Start() {
-        //Create map tiles
-        tiles = new int[sizeX, sizeY];
+	//Players Attatching
+	/*
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject player3;
+*/
+	public bool moveMode = true;
+	int[,] tiles;
+	// int[,] moveTiles;
+	int sizeX = 18;
+	//Manually change for map
+	int sizeY = 18;
+	int tileSizeX = 200;
+	int tileSizeY = 200;
+	int player1Morale = 100, player2Morale = 100;
+	public int health;
+
+	void Start ()
+	{
+		//Create map tiles
+		tiles = new int[sizeX, sizeY];
 		clickTiles = new ClickTile[sizeX, sizeY];
 
-        //Moves tile to tile checking
-        for (int x = 100; x < sizeX * tileSizeX; x += tileSizeX)
-        {
+		//Moves tile to tile checking
+		for (int x = 100; x < sizeX * tileSizeX; x += tileSizeX) {
 
-            for (int y = 100; y < sizeY *tileSizeY; y += tileSizeY)
-            {
-                Debug.Log(background.GetPixel(x, y));
-                //checks if it is green and assigns the grass tile
-                if (background.GetPixel(x, y) == new Color(0,1,0,1))
-                {
-                    tiles[(x-100)/tileSizeX , (y-100)/tileSizeY] = 0;
-                }
+			for (int y = 100; y < sizeY * tileSizeY; y += tileSizeY) {
+				Debug.Log (background.GetPixel (x, y));
+				//checks if it is green and assigns the grass tile
+				if (background.GetPixel (x, y) == new Color (0, 1, 0, 1)) {
+					tiles [(x - 100) / tileSizeX, (y - 100) / tileSizeY] = 0;
+				}
                 //checks if it is blue and assigns the water tile
-                else if (background.GetPixel(x, y) == new Color(0,0,1,1))
-                {
-                    tiles[(x-100)/tileSizeX, (y-100)/tileSizeY] = 1;
-                }
+                else if (background.GetPixel (x, y) == new Color (0, 0, 1, 1)) {
+					tiles [(x - 100) / tileSizeX, (y - 100) / tileSizeY] = 1;
+				}
                 //checks if it is red and assigns the dirt tile
-                else if (background.GetPixel(x, y) == new Color(1,0,0,1))
-                {
-                    tiles[(x-100)/tileSizeX, (y-100)/tileSizeY] = 2;
-                }
-                
-            }
-        }
-        generateMap();
-        generatePlayers();
-    }
-    void generateMap() {
-		clickTiles = new ClickTile[sizeX,sizeY];
-        //goes through the grid
-        for (int x = 0; x < sizeX; x++)
-        {
-            for (int y = 0; y < sizeY; y++)
-            {
-                //finds the tile type and creates a clone of it in the grid section it is assigned to and gives it an x and y coord.
-                TileType tt = tileType[tiles[x, y]];
-                GameObject go = (GameObject)Instantiate(tt.tileVisual, new Vector3(x, y, 0), Quaternion.identity);
-                ClickTile ct = go.GetComponent<ClickTile>();
-                ct.tileX = x;
-                ct.tileY = y;
-                ct.map = this;
-				clickTiles [x,y] = ct;
-            }
-        }
-    }
-    public void generatePlayers() {
-        player1Units = GameObject.FindGameObjectsWithTag("Player 1");
-        player2Units = GameObject.FindGameObjectsWithTag("Player 2");
-    }
-    public void MoveUnitTo(int x, int y) {
+                else if (background.GetPixel (x, y) == new Color (1, 0, 0, 1)) {
+					tiles [(x - 100) / tileSizeX, (y - 100) / tileSizeY] = 2;
+				}
+			}
+		}
+		//Settingthe health for each of the units
+		/*
+		player1.GetComponents ();
+*/
+		generateMap ();
+		generatePlayers ();
+	}
+
+	void generateMap ()
+	{
+		clickTiles = new ClickTile[sizeX, sizeY];
+		//goes through the grid
+		for (int x = 0; x < sizeX; x++) {
+			for (int y = 0; y < sizeY; y++) {
+				//finds the tile type and creates a clone of it in the grid section it is assigned to and gives it an x and y coord.
+				TileType tt = tileType [tiles [x, y]];
+				GameObject go = (GameObject)Instantiate (tt.tileVisual, new Vector3 (x, y, 0), Quaternion.identity);
+				ClickTile ct = go.GetComponent<ClickTile> ();
+				ct.tileX = x;
+				ct.tileY = y;
+				ct.map = this;
+				clickTiles [x, y] = ct;
+			}
+		}
+	}
+
+	public void generatePlayers ()
+	{
+		player1Units = GameObject.FindGameObjectsWithTag ("Player 1");
+		player2Units = GameObject.FindGameObjectsWithTag ("Player 2");
+	}
+
+	public void MoveUnitTo (int x, int y)
+	{
      
-            //checks  to see if it is selected
-            if ((cc.selected && firstPlayerTurn && cc.tag.Equals("Player 1")) || (cc.selected && !firstPlayerTurn && cc.tag.Equals("Player 2")))
-            {
-                //if the distance is within three tiles horizontally or two diagonally move it
-                if (Distance((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= 3)
+		//checks  to see if it is selected
+		if ((cc.selected && firstPlayerTurn && cc.tag.Equals ("Player 1")) || (cc.selected && !firstPlayerTurn && cc.tag.Equals ("Player 2"))) {
+			//if the distance is within three tiles horizontally or two diagonally move it
+			if (Distance ((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= 3)
                     //moves the selected unit. Note the -.75 is for the unit to appear on the grid. It does not move in the z direction
-                    selectedUnit.transform.position = new Vector3(x, y, (float)-0.75);
-            DestroyTiles();
-            cc.selected = false;
-            }
+                    selectedUnit.transform.position = new Vector3 (x, y, (float)-0.75);
+			DestroyTiles ();
+			cc.selected = false;
+		}
 
      
-}
-    public void ChangeUnit(GameObject Cu) {
-        cc = Cu.GetComponent<ClickUnit>();
-        cc.map = this;
-        selectedUnit = Cu;
-        if (cc.selected)
-        {
-            cc.selected = false;
-            DestroyTiles();
-        }
-        else
-        {
-            cc.selected = true;
-            DestroyTiles();
-            CreateTile();
-        }
-    }
-    public double Distance(int x1, int y1, int x2, int y2) {
-        double dis = 0;
-        dis = Mathf.Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2)* (y1 - y2)));
-        return dis;
-    }
-    public void DestroyTiles() {
-        moveTiles = GameObject.FindGameObjectsWithTag("Move");
-        attackTiles = GameObject.FindGameObjectsWithTag("Attack");
-        for (int i = 0; i < moveTiles.Length; i++)
-        {
-            Destroy(moveTiles[i]);
-        }
+	}
 
-        for (int i = 0; i < attackTiles.Length; i++)
-        {
-            Destroy(attackTiles[i]);
-        }
-    }
-    public void CreateTile() {//change so one doesnt spawn on other objects.
-        if ((cc.selected && firstPlayerTurn && cc.tag.Equals("Player 1")) || (cc.selected && !firstPlayerTurn && cc.tag.Equals("Player 2")))
-        {
-            for (int x = 0; x < sizeX; x++)
-            {
-                for (int y = 0; y < sizeY; y++)
-                {
-                    if (Distance((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= cc.maxMoveDistance && Distance((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) > 0 && tileOpen(clickTiles[x,y]))
-                    {
-                        //Debug.Log("X =" + x + "Y=" + y);
-                        GameObject go = (GameObject)Instantiate(TileMove, new Vector3(x, y, (float)-.5), Quaternion.identity);
-                        ClickTile ct = go.GetComponent<ClickTile>();
-                        ct.tileX = x;
-                        ct.tileY = y;
-                        ct.map = this;
+	public void ChangeUnit (GameObject Cu)
+	{
+		cc = Cu.GetComponent<ClickUnit> ();
+		cc.map = this;
+		selectedUnit = Cu;
+		if (cc.selected) {
+			cc.selected = false;
+			DestroyTiles ();
+		} else {
+			cc.selected = true;
+			DestroyTiles ();
+			CreateTile ();
+		}
+	}
 
-                    }
-                    if (Distance((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= cc.attackRange && Distance((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) > 0 && checkEnemy(clickTiles[x, y]))
-                    {
-                        //Debug.Log("X =" + x + "Y=" + y);
-                        GameObject go = (GameObject)Instantiate(TileAttack, new Vector3(x, y, (float)-.5), Quaternion.identity);
-                        ClickTile ct = go.GetComponent<ClickTile>();
-                        ct.tileX = x;
-                        ct.tileY = y;
-                        ct.map = this;
+	public double Distance (int x1, int y1, int x2, int y2)
+	{
+		double dis = 0;
+		dis = Mathf.Sqrt (((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
+		return dis;
+	}
 
-                    }
-                }
-            }
-        }
-    /* for (int x = 0; x < moveTiles.Length; x++) {
+	public void DestroyTiles ()
+	{
+		moveTiles = GameObject.FindGameObjectsWithTag ("Move");
+		attackTiles = GameObject.FindGameObjectsWithTag ("Attack");
+		for (int i = 0; i < moveTiles.Length; i++) {
+			Destroy (moveTiles [i]);
+		}
+
+		for (int i = 0; i < attackTiles.Length; i++) {
+			Destroy (attackTiles [i]);
+		}
+	}
+
+	public void CreateTile ()
+	{//change so one doesnt spawn on other objects.
+		if ((cc.selected && firstPlayerTurn && cc.tag.Equals ("Player 1")) || (cc.selected && !firstPlayerTurn && cc.tag.Equals ("Player 2"))) {
+			for (int x = 0; x < sizeX; x++) {
+				for (int y = 0; y < sizeY; y++) {
+					if (Distance ((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= cc.maxMoveDistance && Distance ((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) > 0 && tileOpen (clickTiles [x, y]) && this.moveMode) {
+						//Debug.Log("X =" + x + "Y=" + y);
+						GameObject go = (GameObject)Instantiate (TileMove, new Vector3 (x, y, (float)-.5), Quaternion.identity);
+						ClickTile ct = go.GetComponent<ClickTile> ();
+						ct.tileX = x;
+						ct.tileY = y;
+						ct.map = this;
+
+					}
+					if (Distance ((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) <= cc.attackRange && Distance ((int)selectedUnit.transform.position.x, (int)selectedUnit.transform.position.y, x, y) > 0 && checkEnemy (clickTiles [x, y]) && !this.moveMode) {
+						//Debug.Log("X =" + x + "Y=" + y);
+						GameObject go = (GameObject)Instantiate (TileAttack, new Vector3 (x, y, (float)-.5), Quaternion.identity);
+						ClickTile ct = go.GetComponent<ClickTile> ();
+						ct.tileX = x;
+						ct.tileY = y;
+						ct.map = this;
+
+					}
+				}
+			}
+		}
+		/* for (int x = 0; x < moveTiles.Length; x++) {
             for (int y = 0; y < moveTiles.Length; y++)
             {
                 if (moveTiles[x, y] == 1)
@@ -165,79 +175,75 @@ public class Map : MonoBehaviour {
             }
         }
      */
-    }
-    public void moraleChange(ClickUnit cu) {
-        if (cu.tag.Equals("Player1")) {
-            player1Morale -= cu.morale;
-        }
-        if (cu.tag.Equals("Player2"))
-        {
-            player2Morale -= cu.morale;
-        }
-    }
-    public bool tileOpen(ClickTile ct) {
-        bool open = true;
-        // search through player units then if x and y are same then not clickable.'
-        for (int i = 0; i < player1Units.Length; i++)
-        {
-            if (player1Units[i].transform.position.x == ct.transform.position.x && player1Units[i].transform.position.y == ct.transform.position.y)
-            {
-                open = false;
-            }
-        }
-        for (int i = 0; i < player2Units.Length; i++) {
+	}
 
-            if (player2Units[i].transform.position.x == ct.transform.position.x && player2Units[i].transform.position.y == ct.transform.position.y)
-            {
-                open = false;
-            }
-        }
+	public void moraleChange (ClickUnit cu)
+	{
+		if (cu.tag.Equals ("Player1")) {
+			player1Morale -= cu.morale;
+		}
+		if (cu.tag.Equals ("Player2")) {
+			player2Morale -= cu.morale;
+		}
+	}
+
+	public bool tileOpen (ClickTile ct)
+	{
+		bool open = true;
+		// search through player units then if x and y are same then not clickable.'
+		for (int i = 0; i < player1Units.Length; i++) {
+			if (player1Units [i].transform.position.x == ct.transform.position.x && player1Units [i].transform.position.y == ct.transform.position.y) {
+				open = false;
+			}
+		}
+		for (int i = 0; i < player2Units.Length; i++) {
+
+			if (player2Units [i].transform.position.x == ct.transform.position.x && player2Units [i].transform.position.y == ct.transform.position.y) {
+				open = false;
+			}
+		}
         
-        return open;
-    }
-    public bool checkEnemy(ClickTile ct) {
-        bool ret = false;
-        bool turn = firstPlayerTurn;
-        if (turn) {
-            for (int i = 0; i < player2Units.Length; i++)
-            {
+		return open;
+	}
 
-                if (player2Units[i].transform.position.x == ct.transform.position.x && player2Units[i].transform.position.y == ct.transform.position.y)
-                {
-                    ret = true;
-                }
-            }
-        }
+	public bool checkEnemy (ClickTile ct)
+	{
+		bool ret = false;
+		bool turn = firstPlayerTurn;
+		if (turn) {
+			for (int i = 0; i < player2Units.Length; i++) {
 
-        else {
-            for (int i = 0; i < player1Units.Length; i++)
-            {
-                if (player1Units[i].transform.position.x == ct.transform.position.x && player1Units[i].transform.position.y == ct.transform.position.y)
-                {
-                    ret = true;
-                }
-            }
-        }
+				if (player2Units [i].transform.position.x == ct.transform.position.x && player2Units [i].transform.position.y == ct.transform.position.y) {
+					ret = true;
+				}
+			}
+		} else {
+			for (int i = 0; i < player1Units.Length; i++) {
+				if (player1Units [i].transform.position.x == ct.transform.position.x && player1Units [i].transform.position.y == ct.transform.position.y) {
+					ret = true;
+				}
+			}
+		}
         
-        return ret;
-    }
+		return ret;
+	}
 
-	public ClickTile getTileOnMap(int x, int y)
+	public ClickTile getTileOnMap (int x, int y)
 	{
 		return clickTiles [x, y];
 	}
 
-	public ClickTile[,] getClickTiles()
+	public ClickTile[,] getClickTiles ()
 	{
 		return clickTiles;
 	}
 
-	public int getSizeX()
+	public int getSizeX ()
 	{
 		return sizeX;
 	}
 
-	public int getSizeY()
+	public int getSizeY ()
 	{
 		return sizeY;
 	}
